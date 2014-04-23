354e9707bd755d7eaefcdb94e4ec5e713eb21451
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-04-23 05:37:44 -0400

    Implementing searching and pulling docker images

--- src/web/cockpit-docker.js
+++ src/web/cockpit-docker.js
@@ -127,10 +127,15 @@ function container_from_cgroup(cgroup) {
 }
 
 function insert_table_sorted(table, row) {
-    var key = $(row).text();
+    insert_table_sorted_generic(table, row, function(row1, row2) {
+        return row1.text().localeCompare(row2.text());
+    });
+}
+
+function insert_table_sorted_generic(table, row, cmp) {
     var rows = $(table).find("tbody tr");
     for (var j = 0; j < rows.length; j++) {
-        if ($(rows[j]).text().localeCompare(key) > 0) {
+        if (cmp($(rows[j]), row) > 0) {
             $(row).insertBefore(rows[j]);
             row = null;
             break;
@@ -267,6 +272,10 @@ PageContainers.prototype = {
                                      { title: _("Running"),             choice: 'running' }
                                    ]);
             $('#containers-containers .panel-heading span').append(this.container_filter_btn);
+            $('#containers-images-search').on("click", function() {
+                  PageSearchImage.display(self.client);
+                  return false;
+              });
         }
 
         var client = get_docker_client();
@@ -500,8 +509,12 @@ PageContainers.prototype = {
         $(row[2]).children("div").attr("value", image.VirtualSize);
         $(row[3]).text($cockpit.format_bytes(image.VirtualSize, 1024).join(" "));
 
-        if (added)
+        if (added) {
+            // Remove downloading row if it exists
+            $("#imagedl_" + image.RepoTags[0].split(':')[0].replace("/", "_")).remove();
+
             insert_table_sorted($('#containers-images table'), tr);
+        }
     },
 
     filter: function() {
@@ -671,6 +684,113 @@ function PageRunImage() {
 }
 
 cockpit_pages.push(new PageRunImage());
+
+PageSearchImage.prototype = {
+    _init: function() {
+        this.id = "containers_search_image_dialog";
+    },
+
+    getTitle: function() {
+        return C_("page-title", "Get new image");
+    },
+
+    show: function() {
+    },
+
+    leave: function() {
+        this.cancel_search();
+    },
+
+    enter: function(first_visit) {
+        var page = this;
+
+        if (first_visit) {
+            $("#containers-search-image-search").on('input', $.proxy(this, "input"));
+            $("#containers-search-image-search").attr( "placeholder", "search by name, namespace or description" );
+            this.search_timeout = null;
+            this.search_request = null;
+        }
+
+        // Clear the previous results and search string from previous time
+        $('#containers-search-image-results tbody tr').remove();
+        $('#containers-search-image-results').hide();
+        $('#containers-search-image-search')[0].value = '';
+    },
+
+    input: function(event) {
+        this.cancel_search();
+
+        // Only handle if the new value is at least 3 characters long
+        if(event.target.value.length < 3)
+            return;
+
+        this.search_timeout = window.setTimeout(this.perform_search, 2000);
+    },
+
+    perform_search: function() {
+        var term = $('#containers-search-image-search')[0].value;
+
+        $('#containers-search-image-waiting').addClass('waiting');
+        $('#containers-search-image-results').hide();
+
+        this.search_request = PageSearchImage.client.search(term).
+          done(function(resp){
+              $('#containers-search-image-waiting').removeClass('waiting');
+              $('#containers-search-image-results tbody tr').remove();
+              $('#containers-search-image-results').show();
+              resp.forEach(function(entry) {
+                  var row = $('<tr>').append(
+                                $('<td>').text(entry.name),
+                                $('<td>').text(entry.description));
+                  row.on('click', function(event) {
+                      PageSearchImage.client.pull(entry.name);
+                      $("#containers_search_image_dialog").modal('hide');
+                  });
+                  row.data('entry', entry);
+
+                  insert_table_sorted_generic($('#containers-search-image-results'), row, function(row1, row2) {
+                      //Bigger than 0 means row1 after row2
+                      //Smaller than 0 means row1 before row2
+                      if (row1.data('entry').is_official && !row2.data('entry').is_official)
+                          return -1;
+                      if (!row1.data('entry').is_official && row2.data('entry').is_official)
+                          return 1;
+                      if (row1.data('entry').is_trusted && !row2.data('entry').is_trusted)
+                          return -1;
+                      if (!row1.data('entry').is_trusted && row2.data('entry').is_trusted)
+                          return 1;
+                      if (row1.data('entry').star_count != row2.data('entry').star_count)
+                          return row2.data('entry').star_count - row1.data('entry').star_count;
+                      return row1.data('entry').name.localeCompare(row2.data('entry').name);
+                  });
+            });
+          }).
+          fail(function(ex){
+              $(this).trigger("failure", [ex]);
+         });
+    },
+
+    cancel_search: function() {
+        window.clearTimeout(this.search_timeout);
+        $('#containers-search-image-results tbody').empty();
+        if (this.search_request !== null) {
+            this.search_request.cancel();
+            this.search_request = null;
+        }
+        $('#containers-search-image-waiting').removeClass('waiting');
+    }
+};
+
+PageSearchImage.display = function(client) {
+    PageSearchImage.client = client;
+    $("#containers_search_image_dialog").modal('show');
+};
+
+function PageSearchImage() {
+    this._init();
+}
+
+cockpit_pages.push(new PageSearchImage());
 
 PageContainerDetails.prototype = {
     _init: function() {
@@ -1063,6 +1183,9 @@ function DockerClient(machine) {
     /* All active poll requests for containers/images indexed by Id */
     var polls = { };
 
+    /* All current download actions */
+    var progress = { };
+
     /*
      * Exposed API, all containers and images
      * Contains the combined /container/json and /container/xxx/json
@@ -1196,6 +1319,28 @@ function DockerClient(machine) {
             $(me).trigger("failure", [ex]);
         });
 
+    /* Pull an image from the central registry
+     */
+    this.pull = function pull(name) {
+        docker_debug("pulling:", name);
+        var tr = $('<tr id="imagedl_' + name.replace("/", "_") + '">').append(
+            $('<td class="container-col-tags">').text(name),
+            $('<td class="container-col-created">').text('Downloading'),
+            $('<td class="image-col-size-graph">').text('Replace with cool bar'),
+            $('<td class="image-col-size-text">'),
+            $('<td class="cell-buttons">'));
+        
+        insert_table_sorted($('#containers-images table'), tr);
+
+        return rest.post("/images/create?fromImage=" + name).
+            stream(function(progress) {
+                //TODO: Do something with progress... or not?
+            }).
+            fail(function(ex) {
+                $(me).trigger("failure", [ex]);
+            });
+    };
+
     /* We listen to the resource monitor and include the measurements
      * in the container objects.
      *
@@ -1321,6 +1466,17 @@ function DockerClient(machine) {
             }).
             done(function(resp) {
                 docker_debug("created:", name, resp);
+            });
+    };
+
+    this.search = function search(term) {
+        docker_debug("searching:", term);
+        return rest.get("/images/search", { "term": term }).
+            fail(function(ex) {
+                docker_debug("search failed:", term, ex);
+            }).
+            done(function(resp) {
+                docker_debug("searched:", term, resp);
             });
     };
 
